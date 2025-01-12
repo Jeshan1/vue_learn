1ccf0913f34ba2a9ffb84b9e579fe419bf24554e
Jeshan1 <jectionjection@gmail.com>
2025-01-13 00:05:13 +0545

    just for fun

--- src/App.vue
+++ src/App.vue
@@ -1,30 +1,30 @@
 <script setup>
-import HelloWorld from './components/HelloWorld.vue'
+import { useRouter, useRoute } from 'vue-router';
+import ButtonWorld from './components/ButtonWorld.vue';
+
+const router = useRouter();
+const route = useRoute()
+const navigateTo = (route) => {
+  router.push(route);
+};
 </script>
 
 <template>
-  <div>
-    <a href="https://vite.dev" target="_blank">
-      <img src="/vite.svg" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://vuejs.org/" target="_blank">
-      <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
-    </a>
+  <div class="container">
+    <!-- Dynamic Buttons -->
+    <div v-if="route.path === '/'">
+      <ButtonWorld label="Login" :onClick="() => navigateTo('/login')" />
+      <ButtonWorld label="Register" :onClick="() => navigateTo('/register')" />
+    </div>
+
+    <!-- Route Content -->
+    <router-view />
   </div>
-  <HelloWorld msg="Vite + Vue" />
 </template>
 
 <style scoped>
-.logo {
-  height: 6em;
-  padding: 1.5em;
-  will-change: filter;
-  transition: filter 300ms;
-}
-.logo:hover {
-  filter: drop-shadow(0 0 2em #646cffaa);
-}
-.logo.vue:hover {
-  filter: drop-shadow(0 0 2em #42b883aa);
+.container {
+  text-align: center;
+  margin-top: 20px;
 }
 </style>
