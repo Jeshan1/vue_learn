d66420a77d648a1e824c12af85aa3a6f2e9c9602
Jeshan1 <jectionjection@gmail.com>
2025-01-13 00:05:14 +0545

    just for fun

---
+++ src/components/ButtonWorld.vue
@@ -0,0 +1,31 @@
+<script setup>
+defineProps({
+  label: {
+    type: String,
+    required: true,
+  },
+  onClick: {
+    type: Function,
+    required: true,
+  },
+});
+</script>
+
+<template>
+  <button @click="onClick">{{ label }}</button>
+</template>
+
+<style scoped>
+button {
+  margin: 10px;
+  padding: 10px 20px;
+  border: 1px solid #ccc;
+  background-color: #f5f5f5;
+  border-radius: 5px;
+  cursor: pointer;
+}
+
+button:hover {
+  background-color: #e5e5e5;
+}
+</style>
