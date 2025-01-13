7412f0ac4ee7c969e80d74d77515b01eb5bd5730
Jeshan1 <jectionjection@gmail.com>
2025-01-13 12:08:48 +0545

    just for fun

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,7 @@
 <script setup>
 import { useRouter, useRoute } from 'vue-router';
 import ButtonWorld from './components/ButtonWorld.vue';
+import Card from './components/Card.vue';
 
 const router = useRouter();
 const route = useRoute()
@@ -15,7 +16,12 @@ const navigateTo = (route) => {
     <div v-if="route.path === '/'">
       <ButtonWorld label="Login" :onClick="() => navigateTo('/login')" />
       <ButtonWorld label="Register" :onClick="() => navigateTo('/register')" />
+      <div class="grid grid-cols-3 gap-5 mx-10">
+        <Card />
+      </div>
     </div>
+
+    
 
     <!-- Route Content -->
     <router-view />
